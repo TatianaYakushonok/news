5cb73ba402e1bedf3a9d3b421b012832457d4749
TatianaYakushonok <yakush2@yandex.ru>
2024-04-09 10:36:51 +0700

    feat: add preload (Tue, 09 Apr 2024 10:36:50 +0700)

--- js/modules/const.js
+++ js/modules/const.js
@@ -1,4 +1,4 @@
-export const URL_ARTICLES = '/js/articles.json';
+export const URL_ARTICLES = 'https://newsapi.org/v2/top-headlines?country=ru';
 export const URL_SEARCH = 'https://newsapi.org/v2/everything/';
 export const main = document.querySelector('.main');
 export const form = document.querySelector('.search-form');
--- js/script.js
+++ js/script.js
@@ -2,39 +2,65 @@ import { URL_ARTICLES, URL_SEARCH, form, main } from './modules/const.js';
 import { createSection } from './modules/createElements.js';
 import { fetchRequest } from './modules/fetchRequest.js';
 import { renderArticles } from './modules/render.js';
+import preload from './modules/preload.js';
 
-const init = async () => {
-  const postItems = createSection('Свежие новости');
-  const articles = await fetchRequest(URL_ARTICLES, {
-    callback: renderArticles,
-  });
-  postItems.innerHTML = '';
-  postItems.append(...articles);
-
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    console.log(data.search);
-    const url = new URL(URL_SEARCH);
-    url.searchParams.append('q', data.search);
-    const postSearch = await fetchRequest(url, {
+const init = () => {
+  preload.show();
+  return Promise.all([
+    fetchRequest(URL_ARTICLES, {
       callback: renderArticles,
       headers: {
         'X-Api-Key': '3480bb0f363a413e8bbd551eb5b74208',
       },
-    });
-    main.innerHTML = '';
+    }),
+  ]);
+};
+
+init().then((data) => {
+  preload.remove();
+  const postItems = createSection('Свежие новости');
+  postItems.innerHTML = '';
+  postItems.append(...data[0]);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  main.innerHTML = '';
+  preload.show();
+  const formData = new FormData(form);
+  const dataPost = Object.fromEntries(formData);
+
+  const url = new URL(URL_SEARCH);
+  url.searchParams.append('q', dataPost.search);
+
+  const init = () => {
+    return Promise.all([
+      fetchRequest(url, {
+        callback: renderArticles,
+        headers: {
+          'X-Api-Key': '3480bb0f363a413e8bbd551eb5b74208',
+        },
+      }),
+      fetchRequest(URL_ARTICLES, {
+        callback: renderArticles,
+        headers: {
+          'X-Api-Key': '3480bb0f363a413e8bbd551eb5b74208',
+        },
+      }),
+    ]);
+  };
+
+  init().then((data) => {
+    preload.remove();
+    form.reset();
     const postList = createSection(
-      `По вашему запросу ${data.search} найдено ${articles.length} результатов`,
+      `По вашему запросу ${dataPost.search} найдено ${data[0].length} результатов`,
     );
-    postList.append(...postSearch);
+    postList.append(...data[0]);
 
     const postTopList = createSection('Свежие новости');
-    const slicedArray = articles.slice(0, 4);
+    const slicedArray = data[1].slice(0, 4);
 
     postTopList.append(...slicedArray);
   });
-};
-
-init();
+});
